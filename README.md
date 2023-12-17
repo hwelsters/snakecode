b429d36bdee7e4abac87d863c22e5b8348b766eb
hwelsters <corevalue.noel@gmail.com>
2023-12-17 10:29:16 -0700

    feat(cdk): made sure environment variables are formatted correctly.

---
+++ README.md
@@ -0,0 +1,9 @@
+# Documentation
+## Setting up
+### CDK
+A file `apps/snakecode-cdk/.env` should be created with the following content.
+
+```bash
+GOOGLE_CLIENT_ID=[YOUR GOOGLE CLIENT ID]
+GOOGLE_CLIENT_SECRET=[YOUR GOOGLE CLIENT SECRET]
+```
---
+++ apps/snakecode-cdk/lib/constants/Env.ts
@@ -0,0 +1,12 @@
+import dotenv from 'dotenv'
+import { cleanEnv, str } from 'envalid'
+
+dotenv.config()
+
+// Log an error message and exit (in Node) if any required env variables are missing
+const Env = cleanEnv(process.env, {
+  GOOGLE_CLIENT_ID: str(),
+  GOOGLE_CLIENT_SECRET: str()
+})
+
+export default Env
