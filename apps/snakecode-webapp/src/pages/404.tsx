4502120c31224059ada518e8c69c490d58e5932a
hwelsters <corevalue.noel@gmail.com>
2024-01-10 22:34:59 -0700

    ci: calculate NextJS bundle size on each pull request.

--- apps/snakecode-webapp/src/pages/404.tsx
+++ apps/snakecode-webapp/src/pages/404.tsx
@@ -1,8 +1,8 @@
 import Link from "next/link";
 
+import Banner from "@/components/common/banner";
 import Navbar from "@/components/common/navbar";
 import routes from "@/constants/routes";
-import Banner from "@/components/common/banner";
 
 export default function Custom404() {
   return (
@@ -10,12 +10,14 @@ export default function Custom404() {
       <Navbar />
 
       <main className="flex h-full w-full flex-col items-center justify-center font-sans text-pt">
-        <span className="-mt-24 text-pc text-center font-sans font-extrabold text-2xl tracking-wide sm:text-2xl">
-          404
-        </span>
+        <span className="-mt-24 text-center font-sans text-2xl font-extrabold tracking-wide text-pc sm:text-2xl">404</span>
         <span className="mt-4 font-display text-5xl">Oh, hello!</span>
-        <span className="mt-4 text-st font-sans font-bold text-xl">Looks like you might have gotten <span className="text-pc">a bit lost</span></span>
-        <Link className="mt-10 font-black text-red text-lg" href={routes.root}>START HERE</Link>
+        <span className="mt-4 font-sans text-xl font-bold text-st">
+          Looks like you might have gotten <span className="text-pc">a bit lost</span>
+        </span>
+        <Link className="mt-10 text-lg font-black text-red" href={routes.root}>
+          START HERE
+        </Link>
       </main>
     </div>
   );
