c010c9d1edbcf4c30046575c88eea0d42c33cab8
hwelsters <corevalue.noel@gmail.com>
2024-01-11 20:57:11 -0700

    refactor(webapp): partially migrated to Next 14's App Router.
    - Migrated most pages to the App Router.

--- apps/snakecode-webapp/src/app/courses/[course]/[section]/page.tsx
+++ apps/snakecode-webapp/src/app/courses/[course]/[section]/page.tsx
@@ -2,7 +2,8 @@ import Navbar from "@/components/common/navbar";
 import CourseSection from "@/components/modules/courses/[course]/[section]/course-section";
 import { getAllStaticPaths, getSection } from "@/utils/content";
 
-export default function Courses({ section }: any) {
+export default function Courses({ params }: { params: { course: string; section: string } }) {
+  const section: any = getSection(params.course, params.section);
   return (
     <div className="bg-bg">
       <Navbar />
@@ -15,17 +16,6 @@ export default function Courses({ section }: any) {
   );
 }
 
-export const getStaticPaths = async () => {
-  return {
-    paths: getAllStaticPaths(),
-    fallback: false,
-  };
-};
-
-export const getStaticProps = async (context: any) => {
-  return {
-    props: {
-      section: getSection(context.params.course, context.params.section),
-    },
-  };
+export const generateStaticParams = async () => {
+  return getAllStaticPaths().map((path) => path.params);
 };
